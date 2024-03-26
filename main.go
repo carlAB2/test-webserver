909fcd5fcfbaa6ec01ec984f267d0aef3bdadfc7
Carl <carl@cyberandao.com>
2024-03-26 23:46:55 +0800

    feat: print server access log

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
@@ -28,20 +30,50 @@ func main() {
 	select {}
 }
 
+func printLog(r *http.Request) {
+	// Print request line, headers, and body to access.log file
+	logFile, err := os.OpenFile("access.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println("Error opening access.log file:", err)
+	}
+	defer logFile.Close()
+	var content = make([]byte, 1024)
+	n, err := r.Body.Read(content)
+	if err != nil {
+		log.Println("Error reading request body:", err)
+	}
+	log.SetOutput(logFile)
+	log.Println("Request Line:", r.Method, r.URL.Path, r.Proto)
+	log.Println("Request Headers:")
+	for k, v := range r.Header {
+		log.Printf("\t%q = %q\n", k, v)
+	}
+	log.Println("Request Body:", string(content[:n]))
+
+	// Print request line, headers, and body to screen
+	fmt.Println("Request Line:", r.Method, r.URL.Path, r.Proto)
+	fmt.Println("Request Headers:")
+	for k, v := range r.Header {
+		fmt.Printf("\t%q = %q\n", k, v)
+	}
+	fmt.Println("Request Body:", string(content[:n]))
+	fmt.Printf("**********************\n")
+}
+
 func fileServer(w http.ResponseWriter, r *http.Request) {
 	filePath := r.URL.Path[len("/fs/"):]
-
 	content, err := readFile(filePath)
 	if err != nil {
-		http.Error(w, "File not found", http.StatusNotFound)
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-
+	printLog(r)
 	w.Write(content)
 }
 
 func readFile(filePath string) ([]byte, error) {
-	content, err := os.ReadFile("./webroot/" + filePath)
+	content, err := ioutil.ReadFile("./webroot/" + filePath)
 	if err != nil {
 		return []byte("read " + filePath + " error: " + err.Error()), err
 	}
