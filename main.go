cd17423612e8b3e0c55d7fc3f04df57c3feeca86
Carl <carl@cyberandao.com>
2024-04-09 18:47:11 +0800

    feat: add cookie test

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ import (
 
 func main() {
 	http.HandleFunc("/fs/", fileServer)
+	http.HandleFunc("/cookie/echo_cookie", echoCookie)
+	http.HandleFunc("/cookie/set_cookie", setCookie)
+	http.HandleFunc("/cookie/delete_cookie", deleteCookie)
 
 	go func() {
 		fmt.Println("HTTP server is running on :8080")
@@ -78,4 +81,36 @@ func readFile(filePath string) ([]byte, error) {
 		return []byte("read " + filePath + " error: " + err.Error()), err
 	}
 	return content, nil
+}
+
+/*
+*   测试chrome浏览器的cookie行为：
+*   测试点1：chrome浏览器发送用户在Application->Cookies中自己添加的cookie
+		1. 在chrome浏览器的开发者工具中，在Application->Cookies中添加一个cookie
+		2. 然后访问http://swt.isanyier.top:8080/cookie/echo_cookie，查看是否能够获取到刚刚添加的cookie
+	测试点2：源站响应Set-Cookie头部，chrome浏览器是否会自动保存cookie
+		1. 访问http://swt.isanyier.top:8080/cookie/set_cookie?cookie_str=cookie
+		2. 在chrome浏览器的开发者工具中，在Application->Cookies中查看是否有对应的cookie
+	测试点3：通过set-cookie头部删除指定cookie
+		1. 访问http://swt.isanyier.top:8080/cookie/delete_cookie?cookie_key=key
+		2. 在chrome浏览器的开发者工具中，在Application->Cookies中查看是否有key的cookie
+*/
+
+func echoCookie(w http.ResponseWriter, r *http.Request) {
+	printLog(r)
+	cookie := r.Header.Values("cookie")
+
+	fmt.Fprintf(w, "%s", cookie)
+}
+
+func setCookie(w http.ResponseWriter, r *http.Request) {
+	printLog(r)
+	setCookie := r.URL.Query().Get("cookie_str")
+	w.Header().Set("Set-Cookie", setCookie)
+}
+
+func deleteCookie(w http.ResponseWriter, r *http.Request) {
+	printLog(r)
+	cookie_key := r.URL.Query().Get("cookie_key")
+	w.Header().Set("Set-Cookie", cookie_key+"=; Max-Age=-1")
 }
